778ef17ae30e9bfe83c865cbaa07e116608036f7
Justin <jrt0614@tamu.edu>
2025-11-20 11:55:00 -0600

    fix

--- src/utils/hanzi.js
+++ src/utils/hanzi.js
@@ -1,26 +1,3 @@
-// import HanziWriter from "hanzi-writer";
-// import { CWIDTH, CHEIGHT } from "../pages/Play";
-// let cachedWriter = null;
-// let cachedDiv = null;
-// export function testtHanziWriter(div, char) {
-//   if (!cachedWriter || cachedDiv !== div) {
-//     cachedDiv = div;
-//     cachedWriter = HanziWriter.create(div, char, {
-//       width: CWIDTH,
-//       height: CHEIGHT,
-//       padding: 20,
-//       showOutline: true,
-//       showCharacter: false,
-//       strokeAnimationSpeed: 2,
-//       delayBetweenStrokes: 300,
-//     });
-//   } else {
-//     cachedWriter.setCharacter(char); // <── cheap, no DOM rebuild
-//   }
-//   return cachedWriter;
-// }
-
-// utils/hanzi.js
 import HanziWriter from "hanzi-writer";
 
 let cachedWriter = null;
